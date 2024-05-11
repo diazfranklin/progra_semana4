161f8e882db16f4d0815006ee60d0d224c20cc70
Franklin Diaz <167331492+diazfranklin@users.noreply.github.com>
2024-05-11 11:41:33 -0600

    Update ejercicioCinco.cs
    Permitie multiplicar cualquier número entre el rago de 0 y 10

--- ejercicioCinco.cs
+++ ejercicioCinco.cs
@@ -5,20 +5,18 @@ public class ejercicioCinco{
         Console.WriteLine("\n\t**********************************************************");
         Console.WriteLine("\t* EJERCICIO 5- TABLA DE MULTIPLICAR *");            
         Console.WriteLine("\t**********************************************************");
-        Console.WriteLine("\t* Entre 0 y 10 *\n");  
+        Console.WriteLine("\t* De un número entre 0 y 10 *\n");  
        
-        Console.Write("\t* Ingrese un número entre 0 y 10: ");
+        Console.Write("\t* Ingrese un número: ");
         int numero = int.Parse(Console.ReadLine());
         
-        if (numero >= 0 && numero <= 10){
-            Console.WriteLine("\n\t* Tabla de multiplicar del número " + numero + ": \n");
+        if (numero >= 0){
+            Console.WriteLine($"\n\t* Tabla de multiplicar del número [{numero}]  \n");
 
             for (int i = 1; i <= 10; i++) {
                 int resultado = numero * i;
-                Console.WriteLine("\t " + numero + " x " + i + " = " + resultado);
+                Console.WriteLine($"\t {numero} x {i} = {resultado} \n");
             }
-        } else {
-            Console.WriteLine("El número ingresado está fuera del rango permitido.");
-        }
+        } 
     }
 }
